ed4604359a3ec5f71bf0918c519658a553136c6e
Nickolay Troshkin <nicktrs@mail.ru>
2023-09-23 23:38:00 +0300

    Создан пустой проект для сервера

---
+++ app/server/PersonServer/src/main/java/com/personservice/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.personservice.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.personservice")
+@Configuration
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
